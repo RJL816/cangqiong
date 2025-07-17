6474d69b850b9d8e9d5078c97698725cb414d373
RJL816 <2943866495@qq.com>
2025-07-17 22:58:10 +0800

    添加test进行测试

---
+++ code/bj44-cosmic-debug/src/main/java/plugins/test.java
@@ -0,0 +1,11 @@
+package plugins;
+
+import kd.bos.base.AbstractBasePlugIn;
+import kd.sdk.plugin.Plugin;
+
+/**
+ * 基础资料插件
+ */
+public class test extends AbstractBasePlugIn implements Plugin {
+
+}
